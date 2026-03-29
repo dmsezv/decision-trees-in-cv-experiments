58392fc9102e9107038c03029056da45947b2eb7
Dima Zverev <dmsezv2@gmail.com>
2026-03-30 01:28:27 +0300

    FIX: dataset name in params and pretty view in mlflow

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,9 +1,11 @@
-def flatten_dict(d: dict, parent_key: str = '', sep: str = '.') -> dict:
+def flatten_dict(d, parent_key='', sep='.'):
     items = []
     for k, v in d.items():
         new_key = f"{parent_key}{sep}{k}" if parent_key else k
         if isinstance(v, dict):
             items.extend(flatten_dict(v, new_key, sep=sep).items())
+        elif isinstance(v, list):
+            items.append((new_key, str(v)))
         else:
             items.append((new_key, v))
     return dict(items)
